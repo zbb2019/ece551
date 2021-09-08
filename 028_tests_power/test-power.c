bb233cd1e3c04681fa1bf9b8a7cb8b81a5b01cee
bz82 <bz82@duke.edu>
2021-09-08 17:37:22 -0400

    028_ added print statements in test-power.c

--- 028_tests_power/test-power.c
+++ 028_tests_power/test-power.c
@@ -3,9 +3,12 @@
 
 unsigned power(unsigned x, unsigned y);
 
-void run_check(unsigned x, unsigned y, unsigned long expected_ans) {
+void run_check(unsigned x, unsigned y, unsigned expected_ans) {
   unsigned ans = power(x, y);
   if (ans != expected_ans) {
+    printf("Test Failed:");
+    printf("x = %d, y = %d\n", x, y);
+    printf("expected:%d\n, ans= %d\n", expected_ans, ans);
     exit(EXIT_FAILURE);
   }
 }
@@ -19,5 +22,6 @@ int main() {
   run_check(2, 6, 64);
   run_check(254, 2, 64516);
   run_check(65000, 2, 4225000000);
+  printf("All test cases passed!!! :/)");
   return EXIT_SUCCESS;
 }
