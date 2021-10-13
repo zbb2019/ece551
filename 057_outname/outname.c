15a13746d3fb95b13e6e2c2a46b67168d4545df5
bz82 <bz82@duke.edu>
2021-10-13 02:31:59 -0400

    057

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,16 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+
+  // outFileName = inputName + ".counts"
+  // add 1 below, b/c strlen() doesnt includ the terminating null character.
+  char * outFileName = malloc((strlen(inputName) + 7 + 1) * sizeof(*outFileName));
+  strcpy(outFileName, inputName);
+  strcat(outFileName, ".counts");
+  return outFileName;
 }
