f189471971caf895c9f71b2b6a3749938d95fbd0
bz82 <bz82@duke.edu>
2021-12-13 10:22:12 -0500

    step 3 - first try

---
+++ 993_eval3_late/cyoa-step3.cpp
@@ -0,0 +1,41 @@
+#include <assert.h>
+#include <ctype.h>
+#include <string.h>
+
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "s3-page.hpp"
+
+// MAIN
+int main(int argc, char ** argv) {
+  // STEP 1 - check the number of inputs
+  if (argc != 2) {
+    std::cerr << "cyoa-step 2: Need exactly 1 input directory!\n";
+  }
+
+  // STEP 2 - Read all the pages (belong to the story into a Story object)
+  Story theStory;
+  theStory.readAllPages(argv[1]);
+  /*
+  // checking step 2 by printing all pages in theStory
+  theStory.printStoryToCheck();
+  */
+
+  // STEP 3 - Checking the story as a whole
+  theStory.checkWholeStory();
+
+  // STEP 4 - calc and display its storyDepth
+  std::vector<int> storyDepth = theStory.calcStoryDepth();
+  theStory.printStoryDepth(storyDepth);
+
+  return EXIT_SUCCESS;
+}
